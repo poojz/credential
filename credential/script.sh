0e98a080563ff9ca1152184333554599013501f3
poojz <31204881+poojz@users.noreply.github.com>
2018-05-30 06:29:09 +0530

    This is the first commit

---
+++ credential/script.sh
@@ -0,0 +1,31 @@
+﻿t= "password=345"
+scan()
+{
+url_without_suffix="${url%.*}"
+reponame="$(basename "${url_without_suffix}")"
+
+b="/*"
+c="$reponame$b"
+echo $c
+for file in $c; do
+echo $(basename "$file")
+echo "the content of the file $(basename "$file") is: "
+f="password([^/]+)"
+for i in `cat $file`
+do
+  
+  m=$i
+  echo "$i"
+  if  [[ $m =~ $f ]]
+  then
+   echo "password found"
+fi	
+done 
+done
+}
+myfunc()
+{
+echo "enter the url of the repository"
+read url
+git clone $url
+}
